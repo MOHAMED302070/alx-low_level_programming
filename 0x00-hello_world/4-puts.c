232b0a4eeafbeffb7a1d93547091ad0cea38278d
MOHAMED302070 <mohamedgemooo@gmail.com>
2023-03-17 12:46:43 -0700

    k

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * description - a c program to print with puts function
+ *
+ *return alwyes =0("sucssful")
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
