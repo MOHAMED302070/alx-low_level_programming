923973614b2ce7b6b8dd25e4cbef1d86906509de
MOHAMED302070 <mohamedgemooo@gmail.com>
2023-03-18 10:45:00 -0700

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+/**
 *main-entry point
 *
-*description:A c program to print with puts function
+*description:A c program to print with printf function
 *
-*return :always 0 (sucsses)
-**/
+*Return: Always 0 (Success)
+*/
 int main(void);
 {
 printf("with proper grammar, but the outcome is a piece of art");
